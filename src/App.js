83d5008ff7d5931151c48111f23392b78a69062c
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-08-09 02:01:37 +0530

    refactored code structure 😋

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Switch, Route, Redirect } from 'react-router'
+
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Navbar from './components/Navbar/Navbar'
+
 
 const App=()=>{
     return(
         <div>
+        <Navbar />
             <p>Hello Markdown Editor</p>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Redirect path="/" />
+            </Switch>
         </div>
     )
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = ()=>{
+    return(
+        <p>Navbar Page</p>
+    )
+}
+
+export default Navbar
